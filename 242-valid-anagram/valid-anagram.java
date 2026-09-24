db1e8b3012e3f805f349c31d16bca8be0fb7cbb6
Kiran-zagade <kiranzagade2005@gmail.com>
2026-09-24 12:27:05 +0530

    Time: 18 ms (13.23%) | Memory: 46.5 MB (24.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,36 +2,30 @@ import java.util.HashMap;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-    if(s.length()!=t.length()){
-        return false;
-    }
-    HashMap<Character,Integer>map=new HashMap<>();
-      for(int i=0;i<s.length();i++){
-        Character ch=s.charAt(i);
-        if(map.containsKey(ch)){
-            map.put(ch,map.get(ch)+1);
-
-      }else{
-        map.put(ch,1);
-      }
-    }
-    for(int i=0;i<t.length();i++) {
-        Character ch=t.charAt(i);
-        if(!map.containsKey(ch)){
+        if(s.length()!=t.length()){
             return false;
         }
-        if(map.containsKey(ch)){
-            map.put(ch,map.get(ch)-1);
-
-        }
-        if(map.get(ch) <0){
-            return false;
-        }
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)+1);
+            }else{
+                map.put(ch,1);
+            }
+        }for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            if(!map.containsKey(ch)){
+                return false;
+            }
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)-1);
+            }
+            if(map.get(ch)<0){
+                return false;
+            }
+        }return true;
     }
-    return true;
-
-    }
-
 }
 
 
